3007b7a452ad3522cfbb371607b6191a810701b4
23069cr <23069cr@hvhs.school.nz>
2026-03-11 22:00:13 +0000

    less imagews

--- song1.js
+++ song1.js
@@ -18,130 +18,121 @@ let C5;
 
 var score = 0;
 function preload() {
-grandPiano = loadImage('images/grandPiano.png');
-cartoonPianoNotes = loadImage('images/pianoNotesCartoon.png');
+  grandPiano = loadImage('images/grandPiano.png');
+  cartoonPianoNotes = loadImage('images/pianoNotesCartoon.png');
 
-//note images//
-c4Image = loadImage('images/c4.png');
-d4Image = loadImage('images/d4.png');
-e4Image = loadImage('images/e4.png');
-f4Image = loadImage('images/f4.png');
-g4Image = loadImage('images/g4.png');
-a4Image = loadImage('images/a4.png');
-b4Image = loadImage('images/b4.png');
-c5Image = loadImage('images/c5.png');
+  //note images//
+  crotchetImg = loadImage('images/crotchet.png');
+  trebleclefImg = loadImage('images/trebleclef.png');
 
-//notes sounds//
-C4 = loadSound("mp3Files/C.mp3");
-Csharp = loadSound("mp3Files/Csharp.mp3");
-D4 = loadSound("mp3Files/D.mp3");
-Dsharp = loadSound("mp3Files/Dsharp.mp3");
-E4 = loadSound("mp3Files/E.mp3");
-F4 = loadSound("mp3Files/F.mp3");
-Fsharp = loadSound("mp3Files/Fsharp.mp3");
-G4 = loadSound("mp3Files/G.mp3");
-Gsharp = loadSound("mp3Files/Gsharp.mp3");
-A4 = loadSound("mp3Files/A.mp3");
-Asharp = loadSound("mp3Files/Asharp.mp3");
-B4 = loadSound("mp3Files/B.mp3");
-C5 = loadSound("mp3Files/HighC.mp3");
+  //notes sounds//
+  C4 = loadSound("mp3Files/C.mp3");
+  Csharp = loadSound("mp3Files/Csharp.mp3");
+  D4 = loadSound("mp3Files/D.mp3");
+  Dsharp = loadSound("mp3Files/Dsharp.mp3");
+  E4 = loadSound("mp3Files/E.mp3");
+  F4 = loadSound("mp3Files/F.mp3");
+  Fsharp = loadSound("mp3Files/Fsharp.mp3");
+  G4 = loadSound("mp3Files/G.mp3");
+  Gsharp = loadSound("mp3Files/Gsharp.mp3");
+  A4 = loadSound("mp3Files/A.mp3");
+  Asharp = loadSound("mp3Files/Asharp.mp3");
+  B4 = loadSound("mp3Files/B.mp3");
+  C5 = loadSound("mp3Files/HighC.mp3");
 }
 
 function setup() {
-	console.log("setup: ");
-cnv = new Canvas(1920, 941);
-//image test//
-e4Note = new Sprite(860, 300, 70, 70, 'k');
-e4Note.image = (e4Image);
-e4Note.visible = true;
-e4Image.resize(400,400);
+  console.log("setup: ");
+  cnv = new Canvas(1920, 941);
+  //image test//
+  trebleclef = new Sprite(860, 300, 70, 70, 'k');
+  trebleclef.image = (trebleclefImg);
+  trebleclefImg.resize(400, 400);
+  trebleclef.visible = true;
 
-d4Note = new Sprite(860, 300, 70, 70, 'k');
-d4Note.image = (d4Image);
-d4Image.resize(400,400);
-d4Note.visible = false;
-//white note sprites//
-C4Rect = new Sprite(500, 750, 100, 300, 'k');
-C4Rect.color = 'white';
+  crotchet = new Sprite(860, 300, 70, 70, 'k');
+  crotchet.image = (crotchetImh);
+  crotchet.visible = true;
+  crotchetImg.resize(400, 400);
 
-D4Rect = new Sprite(600, 750, 100, 300, 'k');
-D4Rect.color = 'white';
 
-E4Rect = new Sprite(700, 750, 100, 300, 'k');
-E4Rect.color = 'white';
+  
+  //white note sprites//
+  C4Rect = new Sprite(500, 750, 100, 300, 'k');
+  C4Rect.color = 'white';
 
-F4Rect = new Sprite(800, 750, 100, 300, 'k');
-F4Rect.color = 'white';
+  D4Rect = new Sprite(600, 750, 100, 300, 'k');
+  D4Rect.color = 'white';
 
-G4Rect = new Sprite(900, 750, 100, 300, 'k');
-G4Rect.color = 'white';
+  E4Rect = new Sprite(700, 750, 100, 300, 'k');
+  E4Rect.color = 'white';
 
-A4Rect = new Sprite(1000, 750, 100, 300, 'k');
-A4Rect.color = 'white';
+  F4Rect = new Sprite(800, 750, 100, 300, 'k');
+  F4Rect.color = 'white';
 
-B4Rect = new Sprite(1100, 750, 100, 300, 'k');
-B4Rect.color = 'white';
+  G4Rect = new Sprite(900, 750, 100, 300, 'k');
+  G4Rect.color = 'white';
 
-C5Rect = new Sprite(1200, 750, 100, 300, 'k');
-C5Rect.color = 'white';
+  A4Rect = new Sprite(1000, 750, 100, 300, 'k');
+  A4Rect.color = 'white';
 
-//black note sprites//
-CsharpRect = new Sprite(550, 700, 50, 200, 'k');
-CsharpRect.color = 'black';
+  B4Rect = new Sprite(1100, 750, 100, 300, 'k');
+  B4Rect.color = 'white';
 
-DsharpRect = new Sprite(650, 700, 50, 200, 'k');
-DsharpRect.color = 'black';
+  C5Rect = new Sprite(1200, 750, 100, 300, 'k');
+  C5Rect.color = 'white';
 
-FsharpRect = new Sprite(850, 700, 50, 200, 'k');
-FsharpRect.color = 'black';
+  //black note sprites//
+  CsharpRect = new Sprite(550, 700, 50, 200, 'k');
+  CsharpRect.color = 'black';
 
-GsharpRect = new Sprite(950, 700, 50, 200, 'k');
-GsharpRect.color = 'black';
-AsharpRect = new Sprite(1050, 700, 50, 200, 'k');
-AsharpRect.color = 'black';
+  DsharpRect = new Sprite(650, 700, 50, 200, 'k');
+  DsharpRect.color = 'black';
+
+  FsharpRect = new Sprite(850, 700, 50, 200, 'k');
+  FsharpRect.color = 'black';
+
+  GsharpRect = new Sprite(950, 700, 50, 200, 'k');
+  GsharpRect.color = 'black';
+  AsharpRect = new Sprite(1050, 700, 50, 200, 'k');
+  AsharpRect.color = 'black';
 }
 
 function keyPressed() {
-//test if e is pressed//
-if (key === 'e') {
-     if (e4Note.visible = true) {
-        score = score + 1; 
-            e4Note.visible = false;
-              d4Note.visible = true;
-    } else {
-        score = score - 1; 
-    }
-}
+  //test if e is pressed//
+  if (key === 'e') {
+    score = score + 1;
+  }
   //white note key presses to make sound//
-    if (key === 'q') {
+  if (key === 'q') {
     C4.play();
   }
-   if (key === 'w') {
+  if (key === 'w') {
     D4.play();
   }
-    if (key === 'e') {
+  if (key === 'e') {
     E4.play();
   }
   if (key === 'r') {
     F4.play();
   }
-   if (key === 't') {
+  if (key === 't') {
     G4.play();
   }
- if (key === 'y') {
+  if (key === 'y') {
     A4.play();
   }
-    if (key === 'u') {
+  if (key === 'u') {
     B4.play();
   }
-if (key === 'i') {
+  if (key === 'i') {
     C5.play();
   }
 
   //black note key presses to make sound//
   if (key === '2') {
     Csharp.play();
-  } 
+  }
   if (key === '3') {
     Dsharp.play();
   }
@@ -159,88 +150,88 @@ if (key === 'i') {
 // draw()
 /*******************************************************/
 function draw() {
-background ('pink')
-text("Score: "+score, 50, 100);
-//white note change colour if key presses//
-if (kb.pressing('q')) {
-C4Rect.color = 'grey';
-} if (kb.released('q')) {
-C4Rect.color = 'white';
-}
+  background('pink')
+  text("Score: " + score, 50, 100);
+  //white note change colour if key presses//
+  if (kb.pressing('q')) {
+    C4Rect.color = 'grey';
+  } if (kb.released('q')) {
+    C4Rect.color = 'white';
+  }
 
-if (kb.pressing('w')) {
-D4Rect.color = 'grey';
-} if (kb.released('w')) {
-D4Rect.color = 'white';
-}
+  if (kb.pressing('w')) {
+    D4Rect.color = 'grey';
+  } if (kb.released('w')) {
+    D4Rect.color = 'white';
+  }
 
-if (kb.pressing('e')) {
-E4Rect.color = 'grey';
-} if (kb.released('e')) {
-E4Rect.color = 'white';
-}
+  if (kb.pressing('e')) {
+    E4Rect.color = 'grey';
+  } if (kb.released('e')) {
+    E4Rect.color = 'white';
+  }
 
-if (kb.pressing('r')) {
-F4Rect.color = 'grey';
-} if (kb.released('r')) {
-F4Rect.color = 'white';
-}
+  if (kb.pressing('r')) {
+    F4Rect.color = 'grey';
+  } if (kb.released('r')) {
+    F4Rect.color = 'white';
+  }
 
-if (kb.pressing('t')) {
-G4Rect.color = 'grey';
-} if (kb.released('t')) {
-G4Rect.color = 'white';
-}
+  if (kb.pressing('t')) {
+    G4Rect.color = 'grey';
+  } if (kb.released('t')) {
+    G4Rect.color = 'white';
+  }
 
-if (kb.pressing('y')) {
-A4Rect.color = 'grey';
-} if (kb.released('y')) {
-A4Rect.color = 'white';
-}
+  if (kb.pressing('y')) {
+    A4Rect.color = 'grey';
+  } if (kb.released('y')) {
+    A4Rect.color = 'white';
+  }
 
-if (kb.pressing('u')) {
-B4Rect.color = 'grey';
-} if (kb.released('u')) {
-B4Rect.color = 'white';
-}
+  if (kb.pressing('u')) {
+    B4Rect.color = 'grey';
+  } if (kb.released('u')) {
+    B4Rect.color = 'white';
+  }
 
-if (kb.pressing('i')) {
-C5Rect.color = 'grey';
-} if (kb.released('i')) {
-C5Rect.color = 'white';
-}
+  if (kb.pressing('i')) {
+    C5Rect.color = 'grey';
+  } if (kb.released('i')) {
+    C5Rect.color = 'white';
+  }
 
 
-//Black note key change colour if key is pressed//
-if (kb.pressing('2')) {
-CsharpRect.color = 'grey';
-} if (kb.released('2')) {
-CsharpRect.color = 'black';
-}
+  //Black note key change colour if key is pressed//
+  if (kb.pressing('2')) {
+    CsharpRect.color = 'grey';
+  } if (kb.released('2')) {
+    CsharpRect.color = 'black';
+  }
 
-if (kb.pressing('3')) {
-DsharpRect.color = 'grey';
-} if (kb.released('3')) {
-DsharpRect.color = 'black';
-}
+  if (kb.pressing('3')) {
+    DsharpRect.color = 'grey';
+  } if (kb.released('3')) {
+    DsharpRect.color = 'black';
+  }
 
-if (kb.pressing('5')) {
-FsharpRect.color = 'grey';
-} if (kb.released('5')) {
-FsharpRect.color = 'black';
-}
+  if (kb.pressing('5')) {
+    FsharpRect.color = 'grey';
+  } if (kb.released('5')) {
+    FsharpRect.color = 'black';
+  }
 
-if (kb.pressing('6')) {
-GsharpRect.color = 'grey';
-} if (kb.released('6')) {
-GsharpRect.color = 'black';
-}
+  if (kb.pressing('6')) {
+    GsharpRect.color = 'grey';
+  } if (kb.released('6')) {
+    GsharpRect.color = 'black';
+  }
 
-if (kb.pressing('7')) {
-AsharpRect.color = 'grey';
-} if (kb.released('7')) {
-AsharpRect.color = 'black';
-}
+  if (kb.pressing('7')) {
+    AsharpRect.color = 'grey';
+  } if (kb.released('7')) {
+    AsharpRect.color = 'black';
+  }
 }
 
 /*******************************************************/
